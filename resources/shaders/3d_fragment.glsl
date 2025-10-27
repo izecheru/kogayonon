2edecb26fc1ca8413942f0db4e3643023bf9879f
Ionut <56791024+izecheru@users.noreply.github.com>
2025-10-27 22:57:00 +0200

    Light is now fading with distance

--- resources/shaders/3d_fragment.glsl
+++ resources/shaders/3d_fragment.glsl
@@ -1,31 +1,37 @@
 #version 460 core
 
-// Inputs
 in vec2 TexCoord;
 in vec3 Normal;
 in vec3 FragPos;
 
-// Textures
 layout(binding = 1) uniform sampler2D u_Texture;
 
-// Outputs
 layout(location = 0) out vec4 FragColor;
 
 void main()
 {
-    float ambient = 0.10f;
-    float specularStrength = 0.5f;
-    vec3 norm = normalize(Normal);
-    vec3 lightPos = vec3(1.0,1.0,1.0);
-    vec3 lightDir = normalize(lightPos-FragPos);
-    vec3 lightColor = vec3(1.0,0.0,0.4);
-    float diff = max(dot(norm,lightDir),0.0);
-    vec3 diffuse = diff * lightColor;
-    vec3 objectColor = texture(u_Texture, TexCoord).rgb;
-    vec3 viewDir = normalize(vec3(0.0f,0.0f,-1.0f) - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+  float ambient = 0.10f;
+  float specularStrength = 0.5f;
+  vec3 norm = normalize(Normal);
+  vec3 lightPos = vec3(1.0,1.0,1.0);
+  vec3 lightVector = lightPos - FragPos;
+  float distance_ = length(lightVector);
+  vec3 lightDir = normalize(lightVector);
+
+  float constant = 1.0;
+  float linear = 0.09;
+  float quadratic = 0.032;
+  float attenuation = 1.0 / (constant + linear * distance_ + quadratic * distance_ * distance_);
+
+
+  vec3 lightColor = vec3(1.0,0.0,0.4);
+  float diff = max(dot(norm,lightDir),0.0);
+  vec3 diffuse = diff * lightColor;
+  vec3 objectColor = texture(u_Texture, TexCoord).rgb;
+  vec3 viewDir = normalize(vec3(0.0f,0.0f,-1.0f) - FragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);  
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+  vec3 specular = specularStrength * spec * lightColor;  
+  vec3 result = (ambient + attenuation * (diffuse + specular)) * objectColor;
+  FragColor = vec4(result, 1.0);
 }
