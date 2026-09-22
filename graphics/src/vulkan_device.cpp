b60745a722b3757b75181ec97255c125d26aa0b0
izecheru <zecheru.ionut.roberto@gmail.com>
2026-09-22 12:43:18 +0300

    refactor: removed weird vulkan debug flags

--- graphics/src/vulkan_device.cpp
+++ graphics/src/vulkan_device.cpp
@@ -188,25 +188,21 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback( VkDebugUtilsMessageSeverity
                                                      const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
                                                      void* pUserData )
 {
-    /*
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT = 0x00000001,
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT = 0x00000010,
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT = 0x00000100,
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT = 0x00001000,
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT = 0x7FFFFFFF
-     */
-    switch ( messageSeverity )
+    if ( messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT )
     {
-    case 1 << 0:
-    case 1 << 4:
-        KINFO( "{}", pCallbackData->pMessage );
-        break;
-    case 1 << 8:
-        KWARN( "{}", pCallbackData->pMessage );
-        break;
-    case 1 << 12:
         KERROR( "{}", pCallbackData->pMessage );
-        break;
+    }
+    else if ( messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT )
+    {
+        KWARN( "{}", pCallbackData->pMessage );
+    }
+    else if ( messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT )
+    {
+        KINFO( "{}", pCallbackData->pMessage );
+    }
+    else if ( messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT )
+    {
+        KINFO( "{}", pCallbackData->pMessage );
     }
 
     return VK_FALSE;
